d76de179db6ab74026aaad7d436da5fd448d0d93
Michael Pope <popemray1986@gmail.com>
2018-07-03 02:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('repo')
+  .then(res => res.json());
+  .then(json => console.log(json));
 }
 
 function getToken() {
